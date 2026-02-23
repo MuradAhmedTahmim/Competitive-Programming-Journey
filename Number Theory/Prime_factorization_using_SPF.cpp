69eccbe93a9ac7ee4b612dd0621afd76fa8190f0
Murad Ahmed Tahmim <muradahmedtahmim@gmail.com>
2026-02-24 01:27:44 +0600

    SPF code added

---
+++ Number Theory/Prime_factorization_using_SPF.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    map<int, int> cnt;
+    for (int i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            cnt[i]++;
+            n /= i;
+        }
+    }
+
+    if (n > 1)
+        cnt[n]++;
+
+    for (auto it : cnt)
+    {
+        cout << it.first << " -> " << it.second << endl;
+    }
+
+    return 0;
+}

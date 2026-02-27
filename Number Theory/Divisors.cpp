2893ccf1fef37319d33b09d89571fefd29cb67ac
Murad Ahmed Tahmim <muradahmedtahmim@gmail.com>
2026-02-28 01:19:40 +0600

    Divisors.cpp file & divisor code added

---
+++ Number Theory/Divisors.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> divs[n + 1];
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j += i)
+        {
+            divs[j].push_back(i);
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " -> ";
+        for (auto val : divs[i])
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
